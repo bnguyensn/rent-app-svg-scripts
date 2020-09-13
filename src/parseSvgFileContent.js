d579499a04349436112911092af2192f32972027
bnguyensn <b.nguyen.sn@gmail.com>
2020-09-13 23:18:16 +0100

    refactor: update parse script

--- src/parseSvgFileContent.js
+++ src/parseSvgFileContent.js
@@ -2,22 +2,28 @@ module.exports = function parseSvgFileContent(content, componentName) {
   // This regex matches everything within an <svg> tag
   const SVG_CONTENT_REGEX = /^\s*(<(g|path|\/g|rect).*)/gm;
 
-  // This regex matches the class="xxx" and id="xxx" attributes
-  const ID_ATTR_REGEX = /(id)="\S+"/gm;
-
-  const CLASS_ATTR_REGEX = /(class=)/gm;
-
   const svgContentMatchResults = content.matchAll(SVG_CONTENT_REGEX);
 
+  // This regex matches the id="xxx" attributes
+  const ID_ATTR_REGEX = /(id)="\S+"/gm;
+
+  // This regex matches the class="xxx" attributes
+  const CLASS_ATTR_REGEX = /(class=)/gm;
+
+  // Each element of this array should be a HTML tag string e.g. <g ...> that
+  // can be put inside an <svg> tag.
   const svgContentTags = [];
   for (const match of svgContentMatchResults) {
+    // Remove unwanted HTML attributes
     const postCleanse1 = match[1].replace(ID_ATTR_REGEX, '');
+
+    // Replace normal HTML attributes their React version
     const postCleanse2 = postCleanse1.replace(CLASS_ATTR_REGEX, 'className=');
 
     svgContentTags.push(postCleanse2);
   }
 
-  const svgContent = svgContentTags.join('\n');
+  const svgContentString = svgContentTags.join('\n');
 
   // This regex matches the viewBox="xxx" attribute
   const VIEWBOX_ATTR_REGEX = /viewBox="((\d|\s|\.)*)"/;
@@ -30,7 +36,7 @@ module.exports = function parseSvgFileContent(content, componentName) {
     `import React from 'react';`,
     `import { SvgIcon, SvgIconProps } from '@material-ui/core';`,
     `export default function ${componentName}(props: SvgIconProps) {`,
-    `return (<SvgIcon viewBox="${viewBox}" {...props}>${svgContent}</SvgIcon>`,
+    `return (<SvgIcon viewBox="${viewBox}" {...props}>${svgContentString}</SvgIcon>`,
     `);}`,
   ];
 
